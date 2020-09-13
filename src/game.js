4d5c4c6a2b78c43d9ebbe5202524f7293a7c605c
maggieyao1211 <momokoyao@hotmail.com>
2020-09-13 14:02:19 -0700

    Add music & sound

--- src/game.js
+++ src/game.js
@@ -1,17 +1,32 @@
 import Minion from './minion';
 import Banana from './banana';
 import Worm from './worm';
+import { backgroundMusic, gameOverMusic, wormHurtSound } from './sounds';
 
 class Game {
   constructor(canvas) {
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
-    this.score = 0;
-    this.kills = 0;
+
+    this.isGameStarted = false;
+
+    this.backgroundMusic = backgroundMusic;
 
     const minionImg = new Image();
     minionImg.src = './dist/assets/minion.png';
     this.minion = new Minion(minionImg);
+
+    this.reset();
+
+    document.addEventListener('keydown', e => this.keyDownHandler(e), false);
+
+    this.animate = this.animate.bind(this)
+    requestAnimationFrame(this.animate);
+  }
+
+  reset() {
+    this.score = 0;
+    this.kills = 0;
 
     this.bananas = [];
 
@@ -20,10 +35,7 @@ class Game {
     this.lastWormBornTime = new Date().getTime() / 1000;
     this.lastWormWithGolfBornTime = new Date().getTime() / 1000;
 
-    document.addEventListener('keydown', e => this.keyDownHandler(e), false);
-
-    this.animate = this.animate.bind(this)
-    requestAnimationFrame(this.animate);
+    this.hasGameOverMusicPlayed = false;
   }
 
   keyDownHandler(e) {
@@ -37,6 +49,18 @@ class Game {
           const banana = new Banana(img, this.minion, keyCode === 74 ? 1 : -1);
           this.bananas.push(banana);
           break;
+        case 32: // 'Space'
+          this.isGameStarted = !this.isGameStarted;
+          if (this.isGameStarted) {
+            this.backgroundMusic.play();
+          } else {
+            this.backgroundMusic.pause();
+          }
+          if (this.minion.health <= 0) {
+            this.minion.health = 500;
+            this.reset();
+          }
+          break;
         default:
           break;
     }
@@ -44,18 +68,36 @@ class Game {
 
   animate(time) {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    if (this.minion.health <= 0) {
+      this.ctx.fillStyle = "Yellow";
+      this.ctx.font = "64px 'Grandstander', cursive";
+      this.ctx.fillText("Game Over", 920, 200);
+      this.ctx.fillText("Score:", 940, 300);
+      this.ctx.fillText(this.score, 1140, 300);
+      this.ctx.fillText("Kills:", 964, 400);
+      this.ctx.fillText(this.kills, 1140, 400);
+      this.backgroundMusic.pause();
+      if (!this.hasGameOverMusicPlayed) {
+        gameOverMusic.play();
+        this.hasGameOverMusicPlayed = true;
+      }
+      this.isGameStarted = false;
+    }
+
+    if (this.isGameStarted) {
     this.minion.draw(this.ctx);
 
     this.ctx.fillStyle = "Yellow";
     this.ctx.font = "20px 'Grandstander', cursive";
-    this.ctx.fillRect(50, 50, this.minion.health, 30);
-    this.ctx.fillText("Minion Health", 50, 35);
+    this.ctx.fillText("Health:", 50, 55);
+    this.ctx.fillRect(125, 35, this.minion.health, 30);
 
-    this.ctx.fillText("Score:", 50, 115);
-    this.ctx.fillText(this.score, 115, 115);
+    this.ctx.fillText("Score:", 50, 95);
+    this.ctx.fillText(this.score, 125, 95);
 
-    this.ctx.fillText("Kills:", 50, 150);
-    this.ctx.fillText(this.kills, 115, 150);
+    this.ctx.fillText("Kills:", 50, 130);
+    this.ctx.fillText(this.kills, 125, 130);
 
     // clear outdated bananas
     const currentTime = new Date().getTime() / 1000;
@@ -87,12 +129,14 @@ class Game {
     this.worms.forEach((worm, i) => {
       this.bananas.forEach(banana => {
         if (worm.collideWithBanana(banana)) {
+          wormHurtSound.play();
           this.deadWorms.push(i);
           this.score += worm.score;
           this.kills += 1;
         }
       });
       if (this.minion.collideWithWorm(worm)) {
+        wormHurtSound.play();
         this.deadWorms.push(i);
         this.minion.health -= 100;
       }
@@ -102,8 +146,11 @@ class Game {
     this.deadWorms = [];
 
     this.worms.forEach(worm => worm.draw(this.ctx));
-
-
+    } else {
+      this.ctx.fillStyle = "Yellow";
+      this.ctx.font = "64px 'Grandstander', cursive";
+      this.ctx.fillText("Press 'Space' to Start/Pause", 670, 500);
+    }
 
     requestAnimationFrame(this.animate);
   }
---
+++ src/sounds.js
@@ -0,0 +1,19 @@
+
+class Sound {
+    constructor(src, volume) {
+        this.sound = new Audio(src);
+        this.sound.volume = volume;
+    }
+
+    play() {
+        this.sound.play();
+    }
+
+    pause() {
+        this.sound.pause();
+    }
+}
+
+export const backgroundMusic = new Sound('./dist/sounds/back_in_toon.mp3', '0.45');
+export const gameOverMusic = new Sound('./dist/sounds/game_over.mp3', '0.45');
+export const wormHurtSound = new Sound('./dist/sounds/worm_hurt.wav.mp3', '0.55');
